911334baad699462378aa008925816988c7ee07f
Benjamin Read <endymion1818@gmail.com>
2018-05-05 16:07:08 +0100

    project init

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'discover-mike-oldfield-content.herokuapp.com/wp/',
+        protocol: 'http',
+        useACF: true,
+        concurrentRequests: 10,
+      }
+    }
+  ],
 }
