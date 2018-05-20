fdae80a25f8a7ac88b8cdecb71816d10c84b7cdb
Benjamin Read <endymion1818@gmail.com>
2018-05-20 18:29:01 +0100

    footer component

---
+++ src/components/footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = ({ siteTitle }) => (
+  <div
+    style={{
+      background: 'black',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,28 +3,17 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
+import Footer from '../components/footer'
 import './index.css'
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
       {children()}
-    </div>
+    <Footer siteTitle={data.site.siteMetadata.title} />
   </div>
 )
 
