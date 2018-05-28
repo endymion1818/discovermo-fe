df44789bf30801fa8f2ce0e47aef64cdd525a544
Benjamin Read <endymion1818@gmail.com>
2018-05-28 22:10:41 +0100

    min height container (temp)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
+  <div
+    style={{
+      minHeight: '75vh'
+    }}
+  >
     <h1>Hello.</h1>
     <p>You've stumbled on a work in progress!</p>
     <p>This will be a new site to help new fans get to know the work of composer, multi-instrumentalist and game designer Mike Oldfield.</p>
