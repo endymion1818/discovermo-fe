f7a962cbaf39d3fd4bf1231949aeda9ca44a09cf
Benjamin Read <endymion1818@gmail.com>
2018-06-18 19:38:01 +0100

    deleted user template temporarily - cannot find AllStrapiUser

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,41 +34,17 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     // Create pages for each article.
     result.data.allStrapiPost.edges.forEach(({ node }) => {
       createPage({
-        path: `/${node.slug}`,
+        path: `/${node.id}`,
         component: path.resolve(`src/templates/post.js`),
         context: {
-          id: node.id,
+          id: node.slug,
         },
       })
     })
   });
 
-  // const getAuthors = makeRequest(graphql, `
-  //   {
-  //     allStrapiUser {
-  //       edges {
-  //         node {
-  //           id
-  //         }
-  //       }
-  //     }
-  //   }
-  //   `).then(result => {
-  //   // Create pages for each user.
-  //   result.data.allStrapiUser.edges.forEach(({ node }) => {
-  //     createPage({
-  //       path: `/authors/${node.id}`,
-  //       component: path.resolve(`src/templates/user.js`),
-  //       context: {
-  //         id: node.id,
-  //       },
-  //     })
-  //   })
-  // });
-
   // Queries for articles and authors nodes to use in creating pages.
   return Promise.all([
     getArticles,
-    // getAuthors,
   ])
 };
