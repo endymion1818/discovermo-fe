87e573f7264f06b4d2a937a5a8d429fb9747e64b
Benjamin Read <endymion1818@gmail.com>
2018-07-24 19:54:43 +0100

    commence homepage build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+import HomeAnimation from ''
 
-const IndexPage = () => (
+const Banner = styled.section`
+  display: flex;
+`
+const HomeIntro = styled.div`
+  background-color: white;
+  display: flex;
+  align-items: center;
+`
+
+const TextTop = styled.p`
+  font-size: 2rem;
+`
+
+const TextMain = styled.h1`
+  font-size: 3rem;
+`
+
+const TextIntro = styled.p`
+  font-size: 1.5rem;
+`
+
+export default IndexPage = props => (
   <div>
-    <h1>Hello.</h1>
-    <p>You've stumbled on a work in progress!</p>
-    <p>This will be a new site to help new fans get to know the work of composer, multi-instrumentalist and game designer Mike Oldfield.</p>
-    <p>Check back in a while for further updates!</p>
-  </div>
+  <Banner>
+    <HomeAnimation/>
+    <HomeIntro>
+      <TextTop>Discover</TextTop>
+      <TextMain>Mike Oldfield</TextMain>
+      <TextIntro>Multi-instrumentalist. Game designer. Genius composer. Discover the incredible world of <Link to="/about">Mike Oldfield.</Link></TextIntro>
+    </HomeIntro>
+  </Banner>
+  
+</div>
 )
-
-export default IndexPage
