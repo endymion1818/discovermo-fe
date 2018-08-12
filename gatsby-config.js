78a21453e792a3a3231191256582567dbac73295
Benjamin Read <endymion1818@gmail.com>
2018-08-12 16:07:09 +0100

    adding heroku location;

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `http://localhost:1337`, // 'https://discovermikeoldfield-content.herokuapp.com/',
         contentTypes: [
           `post`,
           `album`,
