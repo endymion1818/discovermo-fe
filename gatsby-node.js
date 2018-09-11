9ac0558e7e8a87f60b2d30b598b102ba464177cd
Benjamin Read <endymion1818@gmail.com>
2018-09-11 20:30:13 +0100

    finished page programatic creation & list article

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,96 @@
 const path = require(`path`);
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-    createNodeField({
-      node,
-      name: `slug`,
-      value: node.slug,
-    })
-}
+const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
+  resolve(
+    graphql(request).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  
-  return new Promise((resolve, reject) => {
-    graphql(`
+      return result;
+    })
+  )
+});
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const getAlbums = makeRequest(graphql, `
     {
-      allStrapiPost {
+      allStrapiAlbum {
         edges {
           node {
-            title
-            excerpt
-            slug
-            createdAt
             id
+            slug
           }
         }
       }
     }
     `).then(result => {
-      result.data.allStrapiPost.edges.forEach(({ node }) => {
-        createPage({
-          path: '/post/' + node.slug,
-          component: path.resolve(`./src/components/Templates/post.js`),
-          context: {
-            title: node.title,
-            excerpt: node.excerpt,
-            slug: node.slug,
-            createdAt: node.createdAt,
-            id: node.id
-          },
-        })
+    result.data.allStrapiAlbum.edges.forEach(({ node }) => {
+      createPage({
+        path: `/album/${node.slug}`,
+        component: path.resolve(`src/components/Templates/album.js`),
+        context: {
+          id: node.id,
+          slug: node.slug
+        },
       })
-      resolve()
     })
-  })
+  });
 
-}
+  const getPosts = makeRequest(graphql, `
+    {
+      allStrapiPost {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+    `).then(result => {
+    // Create pages for each user.
+    result.data.allStrapiPost.edges.forEach(({ node }) => {
+      createPage({
+        path: `/post/${node.slug}`,
+        component: path.resolve(`src/components/Templates/post.js`),
+        context: {
+          id: node.id,
+          slug: node.slug
+        },
+      })
+    })
+  });
 
+  const getDiscoveries = makeRequest(graphql, `
+  {
+    allStrapiDiscovery {
+      edges {
+        node {
+          id
+          Slug
+        }
+      }
+    }
+  }
+  `).then(result => {
+    result.data.allStrapiDiscovery.edges.forEach(({ node }) => {
+      createPage({
+        path: `/discovery/${node.Slug}`,
+        component: path.resolve(`src/components/Templates/discovery.js`),
+        context: {
+          id: node.id,
+          slug: node.Slug
+        },
+      })
+    })
+  });
+
+  return Promise.all([
+    getAlbums,
+    getPosts,
+    getDiscoveries
+  ])
+};
--- src/components/Templates/discovery.js
+++ src/components/Templates/discovery.js
@@ -1,22 +1,82 @@
-import * as React from 'react'
+import React from 'react'
+import Helmet from 'react-helmet'
 import {graphql} from 'gatsby'
 
+import * as variable from '../variables'
+import { Container, Paragraph, Band, BandSplit } from '../Atoms'
+import PopOut from '../Molecules/Popout'
+import ColumnsOne from '../Organisms/ColumnsOne'
+import ColumnsTwo from '../Organisms/ColumnsTwo'
+
 const DiscoveryTemplate = ({ data }) => (
-  <div>
-    <h1>{data.strapiDiscovery.title}</h1>
-    <p>{data.strapiDiscovery.description}</p>
-  </div>
+  <>
+    <Helmet>
+        <title>{ data.strapiDiscovery.title } | { data.site.siteMetadata.title }</title>
+        <meta name="description" content={ data.strapiDiscovery.about } />
+    </Helmet>
+    <Container>
+      <ColumnsTwo
+        col1={{
+          heading: data.strapiDiscovery.title,
+          content: data.strapiDiscovery.description
+        }}
+        col2={{
+          content: 
+            <div dangerouslySetInnerHTML={{ __html: data.strapiDiscovery.spotify_playlist }}/>
+        }}
+      />
+    </Container>
+    <br/>
+    <BandSplit 
+      id="intro" 
+      bufferTop="0"
+      backgroundColorBottom={variable.BRAND_SECONDARY}
+      backgroundColorTop={variable.BRAND_HILIGHT}>
+      <PopOut>
+        <ColumnsOne
+          narrowView
+          textAlign={'center'}
+          textColor='white'
+          col1={{
+            heading: 'Featured Discovery',
+            content:
+              <>
+                  <Paragraph textColor="white">Recently voted by fans on the Mike Oldfield Facebook page as their favourite album of all time, Ommadawn is undoubtably Mike Oldfield at his best.</Paragraph>
+                  <iframe src="https://open.spotify.com/embed/user/1138668487/playlist/2LTvgOeaIGYIScTO5QjyJi" width="100%" height="80" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+              </>
+          }}
+        />
+      </PopOut>
+    </BandSplit>
+  </>
+  
 )
 
 export default DiscoveryTemplate
 
 export const query = graphql`
-  query DiscoveryTemplate {
-    strapiDiscovery {
+  query DiscoveryTemplate($id: String!) {
+    strapiDiscovery(id: { eq: $id }) {
+      id
+      Slug
       title
       description
       spotify_playlist
       vimeo_video
     }
-  }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allStrapiAlbum(limit:5) {
+      edges {
+        node {
+          title
+          slug
+          coverimg
+          }
+        }
+      }
+    }
 `
