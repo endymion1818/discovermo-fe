05ba64cf5d51f3324a25c49e1082c38520006706
Benjamin Read <endymion1818@gmail.com>
2018-10-11 14:08:28 +0100

    updated dependencies, fixed console warnings

--- src/pages/usage-cookies.js
+++ src/pages/usage-cookies.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {graphql} from 'gatsby'
 import Helmet from 'react-helmet'
 import * as variable from '../components/variables'
 import Layout from '../components/Templates/Layout'
