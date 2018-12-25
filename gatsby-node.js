800566313f9d0a103485a4184e8e2ae7e39d18a6
Benjamin Read <endymion1818@gmail.com>
2018-12-25 20:40:57 +0000

    trying createNode [WIP]

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,24 @@
-const path = require(`path`);
+const path = require(`path`)
 
-const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
-  resolve(
-    graphql(request).then(result => {
-      if (result.errors) {
-        reject(result.errors)
-      }
+const makeRequest = (graphql, request) =>
+  new Promise((resolve, reject) => {
+    resolve(
+      graphql(request).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
 
-      return result;
-    })
-  )
-});
+        return result
+      })
+    )
+  })
 
 exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+  const { createPage } = actions
 
-  const getAlbums = makeRequest(graphql, `
+  const getAlbums = makeRequest(
+    graphql,
+    `
     {
       allStrapiAlbum {
         edges {
@@ -26,20 +29,23 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-    `).then(result => {
+    `
+  ).then(result => {
     result.data.allStrapiAlbum.edges.forEach(({ node }) => {
       createPage({
         path: `/album/${node.slug}`,
         component: path.resolve(`src/components/Templates/album.js`),
         context: {
           id: node.id,
-          slug: node.slug
+          slug: node.slug,
         },
       })
     })
-  });
+  })
 
-  const getPosts = makeRequest(graphql, `
+  const getPosts = makeRequest(
+    graphql,
+    `
     {
       allStrapiPost {
         edges {
@@ -50,7 +56,8 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-    `).then(result => {
+    `
+  ).then(result => {
     // Create pages for each user.
     result.data.allStrapiPost.edges.forEach(({ node }) => {
       createPage({
@@ -58,13 +65,15 @@ exports.createPages = ({ actions, graphql }) => {
         component: path.resolve(`src/components/Templates/post.js`),
         context: {
           id: node.id,
-          slug: node.slug
+          slug: node.slug,
         },
       })
     })
-  });
+  })
 
-  const getDiscoveries = makeRequest(graphql, `
+  const getDiscoveries = makeRequest(
+    graphql,
+    `
   {
     allStrapiDiscovery {
       edges {
@@ -75,22 +84,51 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   }
-  `).then(result => {
+  `
+  ).then(result => {
     result.data.allStrapiDiscovery.edges.forEach(({ node }) => {
       createPage({
         path: `/discovery/${node.Slug}`,
         component: path.resolve(`src/components/Templates/discovery.js`),
         context: {
           id: node.id,
-          slug: node.Slug
+          slug: node.Slug,
         },
       })
     })
-  });
+  })
 
-  return Promise.all([
-    getAlbums,
-    getPosts,
-    getDiscoveries
-  ])
-};
+  return Promise.all([getAlbums, getPosts, getDiscoveries])
+}
+
+exports.onCreateNode = ({
+  node,
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  const { createNode, createNodeField } = actions
+  if (node.internal.type === strapiPost.content) {
+    console.log('found node')
+    const newNode = createNode({
+      id: createNodeId(`${node.id} markdown field`),
+      children: [],
+      parent: node.id,
+      internal: {
+        content: node.markdownField,
+        mediaType: `text/markdown`,
+        contentDigest: createContentDigest(node.markdownField),
+        type: `${node.internal.type}Markdown`,
+      },
+    })
+
+    // Add link to the new node
+    createNodeField({
+      node,
+      name: `markdownNode___NODE`,
+      value: newNode.id,
+    })
+  }
+}
+
+// You can now query the converted markdown field like { NodeTypeWithMarkdownField { fields { markdownNode { childMarkdownRemark { html } } } } }
